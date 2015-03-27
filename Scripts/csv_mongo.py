fc72490125907472f9a103ac21ffc4cc500efb76
Pat Claffey <pat.claffey@brandtone.com>
2015-03-27 16:11:40 +0000

    ETL request logic enhancement

--- Scripts/csv_mongo.py
+++ Scripts/csv_mongo.py
@@ -76,43 +76,41 @@ def convert_row_json2(header_row, line_in, column_type):
             row_in_num += 1
             
     return line_dict
-    
-def run_mongo_etl(db_name, collection_name, csvFile, program_mode, row_limit):
-    '''
-    Purpose: 
-    '''
-    # three program modes are Verify, Insert, Update
-    print(csvFile.getCsvFileName() )
-    row_num = 0
-    error_count = 0
-    process_rows = int(row_limit)
-    filename_csv = ""
-    start_time = datetime.now()
-    
-    client = MongoClient()
-    mongo_database = client[db_name]
-    mongo_collection = mongo_database[collection_name]
 
 
+def import_csv_file( program_mode, csvFile, mongo_collection, row_limit ):
+    
     filename_csv = csvFile.getCsvFileName() 
     header_row = csvFile.getHeader() 
     column_type_in = csvFile.getColumnTypes()
 
+    row_num = 0
+    error_count = 0
+    process_rows = row_limit
 
-    start_time = datetime.now()
-    print('Program start time is ' +  start_time.strftime('%H:%M:%S')   )
-    print(' ')
-
-    with open(filename_csv, 'rt') as f:
+    with open(filename_csv, 'rt') as f:  # open the csv file in order to read it
         for row in f:
-
             row_num += 1
 
-            if row_num == 1: # header row, skip
-                continue
+            if row_num == 1: # this is the header row
+                continue  # do not process the header
+            
+
+
+            if row_num % 50000 == 0:  # print a status message every 50000 records
+                curr_time = datetime.now()
+                print('Rows ' + str(row_num) + ' current time is ' +  curr_time.strftime('%H:%M:%S')   )
+                elapsed_time = curr_time - start_time
+                print('Program elapsed time is ' +  str(elapsed_time  )  )
+
+            if row_num < 5:  # not used
+                pass
+                #print(row_dict)
+
+            if row_num == process_rows:
+                break            
 
             try:
-
                 row_dict = convert_row_json2(header_row, row, column_type_in)
 
                 if program_mode == "Verify":
@@ -131,43 +129,87 @@ def run_mongo_etl(db_name, collection_name, csvFile, program_mode, row_limit):
                     print("Row_num " + str(row_num) + " Error:" , sys.exc_info()[0])
                     print(row)
 
-
-            if row_num == 2:
-                sample_dict1 = row_dict
-
-            if row_num == 3:
-                sample_dict2 = row_dict 
-
-            if row_num % 50000 == 0:
-                curr_time = datetime.now()
-                print('Rows ' + str(row_num) + ' current time is ' +  curr_time.strftime('%H:%M:%S')   )
-                elapsed_time = curr_time - start_time
-                print('Program elapsed time is ' +  str(elapsed_time  )  )
+            if row_num == 2:  
+                sample_dict1 = row_dict  # store sample_row
+            elif row_num == 3:
+                sample_dict2 = row_dict # store sample_row
 
 
-            if row_num < 5:
-                pass
-                #print(row_dict)
-
-            if row_num == process_rows:
-                break
-
-    end_time = datetime.now()
     print(' ')
-    print('Program end time is ' +  end_time.strftime('%H:%M:%S')   )
+    print('Total number of rows processed ' + str(row_num)  )
+    print('Total number of bad rows/errors ' + str(error_count)  )
+
+def report_time(message_in):
+    start_time = datetime.now()
+    print(message_in + ' ' +  start_time.strftime('%H:%M:%S')   )
+    return start_time
+
+def report_elapsed_time(message_in, start_time, end_time ):
     elapsed_time = end_time - start_time
-    print('Program elapsed time is ' +  str(elapsed_time  )  )
-    print(' ')
-    print('Total row numbers ' + str(row_num)  )
-    print('Total errors ' + str(error_count)  )
+    print(message_in + ' ' +  str(elapsed_time  )  )
+    
+def get_mongo_collection( db_name, collection_name):
+    client = MongoClient()
+    mongo_database = client[db_name]
+    mongo_collection = mongo_database[collection_name]
+    return mongo_collection
+
+    
+def run_etl_request(source_type, program_mode,\
+                    csvFile,  row_limit,\
+                    db_name, collection_name):
+    '''
+    Purpose: 
+    '''
+    #log process start time
+    print('')
+    start_time = report_time('Program start time is ')
+    
+    # what is the source
+    source_type = 'Csv_File'
+    source_description = 'CSV File'
+    print('Source type is {}. CSV file name is {}'.format\
+          (source_description, csvFile.getCsvFileName() ) )
+    
+    # what is the target
+    print('Target is MongoDB.  Mongo database name is {}, collection name is {}'.\
+          format( db_name, collection_name ))
+
+
+    # three program modes are Verify, Insert, Update
+    print('Program run mode is {}'.format(program_mode))
+
+
+    # Row limit is set to
+    print('Row limit is set to {}'.format(row_limit))
+    
+
+    #get name of MongoDB collection
+    mongo_collection = get_mongo_collection( db_name, collection_name)
+
+    row_limit_int = int(row_limit)
+    #this is where data is actually read from CSV and loaded into Mongo
+    import_csv_file( program_mode, csvFile, mongo_collection, row_limit_int )
+    
+    print('')    
+    end_time = report_time('Program end time is ')
+    report_elapsed_time('Program elapsed time is ', start_time, end_time )
 
 
 if __name__ == '__main__':
-    db_name = sys.argv[1]
+    '''db_name = sys.argv[1]
     collection_name = sys.argv[2]
     csvFile = sys.argv[3]
     program_mode = sys.argv[4]
-    row_limit = sys.argv[5]
+    row_limit = sys.argv[5]'''
+
+    program_mode = sys.argv[1]  # Verify, Insert, Update
+    source_name = sys.argv[2]  # Csv_File,  Oracle
+    csvFile = sys.argv[3]
+    row_limit = sys.argv[4]
+    db_name = sys.argv[5]
+    collection_name = sys.argv[6]
+
     
     myCsv1 = CsvFileStructure( csvFile )
     run_mongo_etl(db_name, collection_name, myCsv1, program_mode, row_limit)
--- Scripts/test_csv_mongo.py
+++ Scripts/test_csv_mongo.py
@@ -5,6 +5,12 @@ from csv_mongo import *
 class TestBT(unittest.TestCase):
 
     def setUp(self):
+        self.source_name = 'Csv_File'
+        self.db_name = 'any_db'
+        self.collection_name = 'any_connection'
+        self.csvFile = 'test_indonesia.csv'
+        self.program_mode = 'Verify'
+        self.row_limit = 10
         pass
 
 
@@ -12,19 +18,24 @@ class TestBT(unittest.TestCase):
         pass
  
 
+    def test_mongo_connection(self):
+        result = get_mongo_collection( self.db_name, self.collection_name)
+        self.assertIsNotNone( result, 'Connection fails to Mongo Database')
+
     def test_main_verify(self):
-        db_name = 'any_db'
-        collection_name = 'any_connection'
-        csvFile = '/bt/import/test_indonesia.csv'
-        program_mode = 'Verify'
-        row_limit = 10
-    
-        myCsv1 = CsvFileStructure( csvFile )
-        run_mongo_etl(db_name, collection_name, myCsv1, program_mode, row_limit)
+        myCsv1 = CsvFileStructure( self.csvFile )
+        run_etl_request(self.source_name,\
+                        self.program_mode,\
+                        myCsv1,\
+                        self.row_limit,\
+                        self.db_name,\
+                        self.collection_name
+                       )
 
 
 if __name__ == '__main__':
     suite = unittest.TestSuite()
+    suite.addTest( TestBT('test_mongo_connection' ) )
     suite.addTest( TestBT('test_main_verify' ) )
     runner = unittest.TextTestRunner()
     runner.run(suite)
