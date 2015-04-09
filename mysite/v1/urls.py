031bf236c3f7ae2d18cf876a33265c1663aa2468
Pat Claffey <pat.claffey@brandtone.com>
2015-04-09 13:56:35 +0100

    first web service code

--- mysite/v1/urls.py
+++ mysite/v1/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^za/$', views.svc_za, name='za'),
     url(r'^(?P<country_code>\w+)/count/$', views.svc_country, name='country'),
+    url(r'^(?P<country_code>\w+)/msisdn/(?P<msisdn_id>\w+)/$',views.svc_country_msisdn, name='msisdn'),
+    url(r'^(?P<country_code>\w+)/profile/(?P<profile_id>\d+)/$', views.svc_country_profile, name='profile'),
 ]
--- mysite/v1/views.py
+++ mysite/v1/views.py
@@ -8,25 +8,92 @@ import pymongo
 from pymongo import MongoClient
 from collections import OrderedDict
 import json
+from datetime import datetime
 
+
+def date_handler(obj):
+    return obj.isoformat() if hasattr(obj, 'isoformat') else obj
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("You're at the index.")
 
-def svc_za(request):
-    return HttpResponse("Hello, this is South Africa.")
+
+def svc_country_profile(request, country_code, profile_id):
+    client = MongoClient()
+
+    db_name = None
+    db_col_name = None
+
+    if country_code.upper() == 'ID':
+        db_name = 'ID'
+        db_col_name = 'BT_SVC_ID'
+    elif country_code.upper() == 'ZA':
+        db_name = 'ZA'
+        db_col_name = 'BT_SVC_ZA'
+
+
+    db_profile = client[db_name][db_col_name].find_one({"PROFILE_ID":int(profile_id)})
+
+    if db_profile is not None:
+        data_out = OrderedDict(db_profile)
+        output = json.dumps(  data_out, default=date_handler )
+    else:
+        data_out = {}
+        output = {"PROFILE_ID":profile_id}
+
+    return HttpResponse( str(output) )
+
+
+def svc_country_msisdn(request, country_code, msisdn_id):
+    client = MongoClient()
+
+    db_name = None
+    db_col_name = None
+
+    if country_code.upper() == 'ID':
+        db_name = 'ID'
+        db_col_name = 'BT_SVC_ID'
+
+    elif country_code.upper() == 'ZA':
+        db_name = 'ZA'
+        db_col_name = 'BT_SVC_ZA'
+
+
+    db_msisdn = client[db_name][db_col_name].find_one({"_id":msisdn_id})
+
+    if db_msisdn is not None:
+        data_out = OrderedDict(db_msisdn)
+        output = json.dumps(  data_out, default=date_handler )
+    else:
+        data_out = {}
+        output = {}
+
+    return HttpResponse( str(output) )
 
 def svc_country(request, country_code):
     client = MongoClient()
+
+    if country_code.upper() == 'ID':
+        db_name = 'ID'
+        db_col_name = 'BT_SVC_ID'
+        db_grand_total = str(client[db_name][db_col_name].count())
+    elif country_code.upper() == 'ZA':
+        db_name = 'ZA'
+        db_col_name = 'BT_SVC_ZA'
+        db_grand_total = str(client[db_name][db_col_name].count())
+    else :
+        db_name = None
+        db_col_name = None
+        db_grand_total = None
+
     dbs = client.database_names()
     dbs_values = ' '.join(dbs) 
     data_out = OrderedDict()
     data_out['country_code'] = country_code
-    data_out['database'] = country_code.upper() 
-    data_out['collection'] = 'DW_SVC_' + country_code.upper() 
-    data_out['count'] = None
+    data_out['database'] =  db_name
+    data_out['collection'] =  db_col_name 
+    data_out['count'] = db_grand_total
     
 
     output = json.dumps(  data_out)
-    #return HttpResponse("Hello %s, this is country code <b>%s</b>" % (dbs_values , country_code) )
     return HttpResponse( str(output) )
